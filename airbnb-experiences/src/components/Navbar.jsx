80981599eefd46daa8f438c421483bd3607cb1f7
Abhishek Kasni <46194594+Abhishekkasni@users.noreply.github.com>
2023-07-31 17:12:56 +0530

    Delete airbnb-experiences directory

--- airbnb-experiences/src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-export default function Navbar() {
-  return (
-    <div id='navbar'>
-      <nav>
-        <img id='airbnb-logo' src="airbnb-logo.png" alt="AirBnB logo" />
-      </nav>
-    </div>
-  )
-}
